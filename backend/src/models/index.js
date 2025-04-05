354a9b52a21844b076f26d89468c0d037e20e18a
Shubhamashish1310 <98949989+Shubhamashish1310@users.noreply.github.com>
2025-04-05 23:23:25 +0530

    add sequelize-cli dependency, update .gitignore, and implement Sequelize model initialization

---
+++ backend/src/models/index.js
@@ -0,0 +1,51 @@
+'use strict';
+
+import fs from 'fs';
+import path from 'path';
+import { Sequelize } from 'sequelize';
+import process from 'process';
+import { fileURLToPath } from 'url';
+import configJson from '../config/config.json' assert { type: 'json' };
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || 'development';
+const config = configJson[env];
+const db = {};
+
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
+
+fs
+  .readdirSync(__dirname)
+  .filter(file => {
+    return (
+      file.indexOf('.') !== 0 &&
+      file !== basename &&
+      file.slice(-3) === '.js' &&
+      file.indexOf('.test.js') === -1
+    );
+  })
+  .forEach(file => {
+    const model = import(path.join(__dirname, file)).then(module => module.default(sequelize, Sequelize.DataTypes));
+    model.then(m => {
+      db[m.name] = m;
+    });
+  });
+
+Object.keys(db).forEach(modelName => {
+  if (db[modelName]?.associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+export default db;
